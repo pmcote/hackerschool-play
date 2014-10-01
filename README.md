a79f489098d1d231ebf8cd59c62ac799395510c1
pmcote <pmcote1@gmail.com>
2014-10-01 16:13:23 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A place to experiment with qualitative data visualization.
---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express'); 		
+var app = express(); 
+
+app.configure(function(){
+	app.set('port', process.env.PORT || 3000)
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'))
+	//mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/[]');
+})
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
